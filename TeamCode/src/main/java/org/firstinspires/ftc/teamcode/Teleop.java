4817be9aaa160267fd1118023f694f53e4ff2169
jdhar <jdharlow10@gmail.com>
2023-12-27 19:06:31 -0500

    Started working on some smaller fixes, slowing drivetrain, adding drone launcher(not done), stopping servos from running accidentally

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "A Teleop", group = "Teleop")
 public class Teleop extends LinearOpMode
@@ -17,25 +18,38 @@ public class Teleop extends LinearOpMode
             robot.robotODrive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
 
             //Transfer Code --
-            //Slides go up, joystick backwards for some reason
-            if (gamepad2.left_stick_y < -.05)
-            {
-                robot.transferM1.setPower(-1);
-                robot.transferM2.setPower(1);
+            //slides go up proportionally to stick value
+            if (gamepad2.left_stick_y < -.05) {
+                robot.transferM1.setPower(-gamepad2.left_stick_y);
+                robot.transferM2.setPower(gamepad2.left_stick_y);
             }
-            //slides go down joystick still backwards
-            else if (gamepad2.left_stick_y > .05 && robot.transferM1.getCurrentPosition() < -50)
-            {
-                robot.transferM1.setPower(.25);
-                robot.transferM2.setPower(-.25);
+            //Slides go down at full power
+            else if(gamepad2.left_stick_y > .05) {
+                robot.transferM1.setPower(-.85);
+                robot.transferM2.setPower(.85);
             }
-            //slides hold
-            else
-            {
+            //zeropowerbehaviour redundancy
+            else {
                 robot.transferM1.setPower(0);
                 robot.transferM2.setPower(0);
             }
             // -- End Transfer code
+
+
+
+
+            //Drone Launcher Code --
+            //Spins the servo for 1 second
+            if (gamepad2.b) {
+                //Creates an elapsed timer
+                ElapsedTime timer = new ElapsedTime();
+                timer.startTime();
+                //While 1 second mark has not passed
+                while(timer.seconds() < 1)
+                    robot.droneLaunch.setPower(1);
+                robot.droneLaunch.setPower(0); //Redundancy for zeroPowerBehaviour
+            }
+            //-- End Drone Launcher Code
 
             //Intake code --
             //Spin Inwards
@@ -57,18 +71,18 @@ public class Teleop extends LinearOpMode
                 robot.intakeMotor.setPower(0);
                 robot.intakeServo.setPower(0);
                 robot.transferCR1.setPower(0);
-                robot.transferCR1.setPower(0);
+                robot.transferCR2.setPower(0);
             }
             // -- End Intake Code
 
 
             //Deposit code --
             if (gamepad2.right_trigger > 0.05)
-                robot.depositServoOne.setPosition(1);
+                robot.depositServoOne.setPosition(.5);
              else
                 robot.depositServoOne.setPosition(0);
             if (gamepad2.left_trigger > 0.05)
-                robot.depositServoTwo.setPosition(1);
+                robot.depositServoTwo.setPosition(.5);
              else
                 robot.depositServoTwo.setPosition(0);
             // -- End Deposit Code
